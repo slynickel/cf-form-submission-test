e8e39d9c9d7c8ba0bfcdd021596f330d2201a0da
Nick Krueger <slynickel.github@gmail.com>
2022-11-27 20:53:07 -0600

    Add write API

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,12 +2,13 @@
  * POST /api/submit
  */
  export async function onRequestPost({ request }) {
+	let pk = "";
 	try {
 		let input = await request.formData();
 
 		// Convert FormData to JSON
 		// NOTE: Allows mutliple values per key
-		let tmp, output = {};
+		let tmp, output, email = {};
 		for (let [key, value] of input) {
 			tmp = output[key];
 			if (tmp === undefined) {
@@ -15,9 +16,25 @@
 			} else {
 				output[key] = [].concat(tmp, value);
 			}
+			tmp = output["email"];
+			if (tmp != undefined) {
+				pk = tmp
+			}
+		}
+
+		if (pk === undefined) {
+			// TODO error handling
+			pk = "noemail"
 		}
 
 		let pretty = JSON.stringify(output, null, 2);
+
+		try {
+			await CF_FORMDATA.put(pk,output)
+		} catch (err) {
+			return new Response('Error writing submission', {status: 400});
+		}
+
 		return new Response(pretty, {
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8'
